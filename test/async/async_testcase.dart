d9e0cae891ae802b01d337b8cad714e16e439039
MGraf <maximiliangrafadelmann@gmx.de>
2021-01-07 11:02:24 +0100

    add more variants

--- test/async/async_testcase.dart
+++ test/async/async_testcase.dart
@@ -3,13 +3,61 @@ import 'package:test_tester/async/async.dart';
 
 abstract class AsyncTestcase {
   void validate() {
-    test('throwExeptionAfter', () async {
-      var impl = await instance();
+    group('throwExeptionAfter', () {
+      test('simple call without waiting', () async {
+        var impl = await instance();
 
-      expect(impl.throwExceptionAfter(1), throwsException);
+        expect(impl.throwExceptionAfter(0), throwsException);
 
-      var count = impl.count();
-      expect(count, 1);
+        await Future.delayed(Duration(seconds: 1));
+
+        var count = impl.count();
+        expect(count, 1);
+      });
+      test('simple call', () async {
+        var impl = await instance();
+
+        expect(impl.throwExceptionAfter(1), throwsException);
+
+        var count = impl.count();
+        expect(count, 1);
+      });
+      test('simple call with completion', () async {
+        var impl = await instance();
+
+        expect(impl.throwExceptionAfter(1), completion(throwsException));
+
+        var count = impl.count();
+        expect(count, 1);
+      });
+      test('call as a function', () async {
+        var impl = await instance();
+
+        expect(() => impl.throwExceptionAfter(1), throwsException);
+
+        var count = impl.count();
+        expect(count, 1);
+      });
+
+      test('call as a explicit async function with await', () async {
+        var impl = await instance();
+
+        expect(() async {
+          await impl.throwExceptionAfter(1);
+        }, throwsException);
+
+        var count = impl.count();
+        expect(count, 1);
+      });
+
+      test('simpel call expectLater', () async {
+        var impl = await instance();
+
+        expectLater(impl.throwExceptionAfter(1), throwsException);
+
+        var count = impl.count();
+        expect(count, 1);
+      });
     });
   }
 
